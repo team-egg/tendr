40ea87246563221478b8d004a267bd68a8cea6e3
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-06-08 18:20:55 -0700

    function definitions and API links, sample code

---
+++ sample.py
@@ -0,0 +1,15 @@
+import requests
+
+## search company name
+def get_symbol(symbol):
+    url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(symbol)
+
+    result = requests.get(url).json()
+
+    for x in result['ResultSet']['Result']:
+        if x['symbol'] == symbol:
+            return x['name']
+
+    company = get_symbol("MSFT")
+
+    print(company)

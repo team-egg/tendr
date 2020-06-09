244d58e633b01d0dc0e63f02949e18f951038a81
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-06-09 14:37:33 -0700

    search w test string

--- news.py
+++ news.py
@@ -1,13 +1,16 @@
 from googlesearch import search 
 
-#from /eugenes_file/file.py import company_variable as company
+#from eugenes_file.py.module import company_return_variable as company
 
 #search key id
 # get company name
 
 #search company name
 # company = company
-company = 'Apple'
+company = 'Microsoft'
+
+check_str = isinstance(company, str)
+print_check = print('*** is company a string? : ' + str(check_str) + ' ***')
 
 add_news = ' news'
 query = company + add_news
@@ -17,6 +20,7 @@ view_query = f"""
 *** search query : {query} ***
 {"*"*40}"""
 
+url_list =[]
 def search_company(query):
     """takes in a company name string and searches google"""
     print(view_query)
@@ -24,10 +28,10 @@ def search_company(query):
     #search ( , domain=google.com or .in, num=how many, stop=last result, pause=cooldown)
     for j in search(query, tld="co.in", num=10, stop=10, pause=2): 
         print(j)
+        url_list.append(j)
+    # print(url_list)
+    return url_list
 
-
+#give url list to pengs file to grab text and analyze
 if __name__ == "__main__":
     search_company('Microsoft news')
-    search_company('Apple news')
-
-

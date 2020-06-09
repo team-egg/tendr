004091c6ba299b1e91d0ebd814a23cd76ed4d3c4
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-06-09 13:31:45 -0700

    10 news URls from main google

--- news.py
+++ news.py
@@ -1,19 +1,33 @@
 from googlesearch import search 
 
-#search key id
-#get company name
+#from /eugenes_file/file.py import company_variable as company
 
+#search key id
+# get company name
 
 #search company name
-company = 'Microsoft'
+# company = company
+company = 'Apple'
 
-def search_company(company):
+add_news = ' news'
+query = company + add_news
+
+view_query = f"""
+{"*"*40}
+*** search query : {query} ***
+{"*"*40}"""
+
+def search_company(query):
     """takes in a company name string and searches google"""
+    print(view_query)
 
-    for j in search(company, tld="co.in", num=10, stop=10, pause=2): 
+    #search ( , domain=google.com or .in, num=how many, stop=last result, pause=cooldown)
+    for j in search(query, tld="co.in", num=10, stop=10, pause=2): 
         print(j)
 
 
 if __name__ == "__main__":
-    search_company('Microsoft')
+    search_company('Microsoft news')
+    search_company('Apple news')
+
 
